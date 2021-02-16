d340d40b89936b75c0811d32fa92527d40c18706
arvinxx <arvinx@foxmail.com>
2021-02-16 21:59:03 +0800

    :white_check_mark: test: 调整测试用例

--- packages/api-baidu/tests/index.spec.ts
+++ packages/api-baidu/tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { Baidu } from '../src';
 
 describe('Baidu 翻译接口', () => {
-  // Please refer to http://api.fanyi.baidu.com/api/trans/product/prodinfo
+  // API 地址 http://api.fanyi.baidu.com/api/trans/product/prodinfo
 
   const baidu = new Baidu({
     config: {
@@ -10,46 +10,47 @@ describe('Baidu 翻译接口', () => {
     },
   });
 
-  describe('', () => {});
-  it('should translate en2zh successfully', async () => {
-    const En2Zh = await baidu.translate('I love you', 'auto', 'zh-CN');
+  describe('正常调用', () => {
+    it('英翻中正常', async () => {
+      const data = await baidu.translate('I love you', 'auto', 'zh-CN');
 
-    expect(En2Zh).toEqual({
-      success: true,
-      type: 'baidu',
-      text: 'I love you',
-      from: 'en',
-      to: 'zh-CN',
-      /** 原文 */
-      origin: {
-        paragraphs: ['I love you'],
-      },
-      /** 译文 */
-      trans: {
-        paragraphs: ['我爱你'],
-      },
-    });
-  }, 9000);
+      expect(data).toEqual({
+        success: true,
+        type: 'baidu',
+        text: 'I love you',
+        from: 'en',
+        to: 'zh-CN',
+        /** 原文 */
+        origin: {
+          paragraphs: ['I love you'],
+        },
+        /** 译文 */
+        trans: {
+          paragraphs: ['我爱你'],
+        },
+      });
+    }, 9000);
 
-  it('should translate zh2en successfully', async () => {
-    const zh2En = await baidu.translate('我爱你', 'auto', 'en');
+    it('中翻英正常', async () => {
+      const data = await baidu.translate('我爱你', 'auto', 'en');
 
-    expect(zh2En).toEqual({
-      success: true,
-      type: 'baidu',
-      text: '我爱你',
-      from: 'zh-CN',
-      to: 'en',
-      /** 原文 */
-      origin: {
-        paragraphs: ['我爱你'],
-      },
-      /** 译文 */
-      trans: {
-        paragraphs: [expect.stringContaining('I love you')],
-      },
-    });
-  }, 9000);
+      expect(data).toEqual({
+        success: true,
+        type: 'baidu',
+        text: '我爱你',
+        from: 'zh-CN',
+        to: 'en',
+        /** 原文 */
+        origin: {
+          paragraphs: ['我爱你'],
+        },
+        /** 译文 */
+        trans: {
+          paragraphs: [expect.stringContaining('I love you')],
+        },
+      });
+    }, 9000);
+  });
 
   it('should get supported languages', () => {
     const result = baidu.getSupportLanguages();
